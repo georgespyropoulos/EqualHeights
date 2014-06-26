6da7ace194bbe7629296b115933189e3110b211e
George <georgesinbox@gmail.com>
2014-06-26 15:37:59 +1000

    Update EqualHeights.js

--- EqualHeights.js
+++ EqualHeights.js
@@ -1,16 +1,14 @@
-$(function(){
-	(equalHeights = function() {	
-		if ($('[data-equal-heights]').length) {
-			$('[data-equal-heights]').parent().each(function(){
-				var element_height = 0;
-				$(this).find('[data-equal-heights]').each(function(){
-					$(this).removeAttr('style');
-					if ($(this).outerHeight() > element_height) {
-						element_height = $(this).outerHeight();
-					} 
-				});
-				$(this).find('[data-equal-heights]').css('min-height',element_height + 'px');
+(equalHeights = function() {	
+	if ($('[data-equal-heights]').length) {
+		$('[data-equal-heights]').parent().each(function(){
+			var element_height = 0;
+			$(this).find('[data-equal-heights]').each(function(){
+				$(this).removeAttr('style');
+				if ($(this).outerHeight() > element_height) {
+					element_height = $(this).outerHeight();
+				} 
 			});
-		}
-	})();
-});
+			$(this).find('[data-equal-heights]').css('min-height',element_height + 'px');
+		});
+	}
+})();
